5ad9f6d7abd6ce58167eeea49dc10ca094d2e359
ZwyklyBazyl <jarek.kijaczko@gmail.com>
2020-04-16 00:21:18 +0200

    Initial commit

---
+++ FloatingText/MovingText.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FloatingText
+{
+    public partial class MovingText: UserControl
+    {
+        int x, y;
+
+        public MovingText()
+        {
+            InitializeComponent();
+            label1.Text = "XDDD";
+            label1.Font = new Font(" ", 22, FontStyle.Bold);
+            timer1.Interval = 1;
+            timer1.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.SetBounds(x, y, 1, 1);
+            x++;
+            if(x>=1000)
+            {
+                x = 0;
+            }
+        }
+    }
+}
